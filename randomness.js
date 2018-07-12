95ef2339ace02dc00a0b933a090ca7f3f2de2e27
manuelASC5 <manuelnajera1119@gmail.com>
2018-07-12 11:26:45 -0400

    work

---
+++ randomness.js
@@ -0,0 +1,50 @@
+
+
+/*function number(){
+    let x = Math.random();
+    randomNumber = x*22;
+    console.log(randomNumber);
+}
+number();*/
+
+/*let alphabet = "abcdefghijklmnopqrstuvwxyz";
+function letters(){
+    let x = Math.random();   
+    let randomNumber = x*27; 
+    randomNumber = Math.floor(randomNumber);
+    console.log (alphabet[randomNumber]);
+}
+letters();*/
+
+/*let foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+function randomfoods(){
+    let x = Math.random();   
+    let randomNumber = x*7; 
+    randomNumber = Math.floor(randomNumber);
+    console.log (foods[randomNumber]);
+}
+randomfoods();*/
+
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+function kids(){
+    let x = Math.random();
+    let randomNumber = x*8;
+    randomNumber = Math.floor(randomNumber);
+    console.log (students[randomNumber])
+}
+kids();
